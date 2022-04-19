89838137c8f822c5157f3d05fbc2c090cd564284
technomaniac00 <101005124+technomaniac00@users.noreply.github.com>
2022-04-19 20:11:21 +0530

    Add files via upload

---
+++ php/query.php
@@ -0,0 +1,10 @@
+<?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("singh.aditya28@outlook.com","My subject",$msg);
+?>
