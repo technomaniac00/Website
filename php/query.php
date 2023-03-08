0e620d820c7c34b8aa628b30cc314b6472153c96
technomaniac00 <singh.aditya28@outlook.com>
2023-03-08 09:48:16 +0530

    php code

--- php/query.php
+++ php/query.php
@@ -1,10 +1,18 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
+if(isset($_POST['submit'])){
+    $to = "singh.aditya28@outlook.com"; // this is your Email address
+    $from = $_POST['mail']; // this is the sender's Email address
+    $first_name = $_POST['fullname'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("singh.aditya28@outlook.com","My subject",$msg);
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
